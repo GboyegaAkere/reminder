db2ee392490f4a3149be63b9db41629352c77ca7
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2022-12-05 20:41:02 +0100

    new changes

--- src/data.jsx
+++ src/data.jsx
@@ -1,7 +1,7 @@
 export default [
     {
       id: 1,
-      name: 'Bertie Yates',
+      name: 'Betty charles',
       age: 29,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
